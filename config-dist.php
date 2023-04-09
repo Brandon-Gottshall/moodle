9b429ea8e7691740c7f2448db19748cc2e19f793
Brandon Gottshall <55668392+Brandon-Gottshall@users.noreply.github.com>
2023-04-09 05:48:10 -0400

    Update config-dist.php

--- config-dist.php
+++ config-dist.php
@@ -137,7 +137,7 @@ $CFG->dboptions = array(
 // If you need both intranet and Internet access please read
 // http://docs.moodle.org/en/masquerading
 
-$CFG->wwwroot   = 'http://localhost/';
+$CFG->wwwroot   = 'PLACEHOLDER_WWWROOT';
 
 
 //=========================================================================
