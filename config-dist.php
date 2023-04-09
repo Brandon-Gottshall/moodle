f272e3769d7abb05e90d8300c9e88730c2ab19d2
Brandon Gottshall <55668392+Brandon-Gottshall@users.noreply.github.com>
2023-04-09 04:22:47 -0400

    Update config-dist.php

--- config-dist.php
+++ config-dist.php
@@ -38,12 +38,12 @@ $CFG = new stdClass();
 // will be stored.  This database must already have been created         //
 // and a username/password created to access it.                         //
 
-$CFG->dbtype    = 'pgsql';      // 'pgsql', 'mariadb', 'mysqli', 'auroramysql', 'sqlsrv' or 'oci'
+$CFG->dbtype    = '${MOODLE_DB_TYPE}';      // 'pgsql', 'mariadb', 'mysqli', 'auroramysql', 'sqlsrv' or 'oci'
 $CFG->dblibrary = 'native';     // 'native' only at the moment
 $CFG->dbhost    = 'localhost';  // eg 'localhost' or 'db.isp.com' or IP
-$CFG->dbname    = 'moodle';     // database name, eg moodle
-$CFG->dbuser    = 'username';   // your database username
-$CFG->dbpass    = 'password';   // your database password
+$CFG->dbname    = '${MOODLE_DB_NAME}';     // database name, eg moodle
+$CFG->dbuser    = '${MOODLE_DB_USER}';   // your database username
+$CFG->dbpass    = '${MOODLE_DB_PASSWORD}';   // your database password
 $CFG->prefix    = 'mdl_';       // prefix to use for all table names
 $CFG->dboptions = array(
     'dbpersist' => false,       // should persistent database connections be
